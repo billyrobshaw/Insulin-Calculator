aba00fe70a7b1c10198fe72c29fa0db478e83679
Billy <billyrobshaw2k3@gmail.com>
2025-03-15 17:36:02 +0000

    first commit

---
+++ Insulin-Converter/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Insulin_Converter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new HomePage());
+        }
+    }
+}
---
+++ Insulin-Converter/HomePage.xaml.cs
@@ -0,0 +1,11 @@
+namespace Insulin_Converter;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+
+	public Thickness BorderThickness { get; set; }
+}
